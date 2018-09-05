3e96764b5ccad775eea65defd3cd2247e72bbb91
Glauber <jose.oliveira@ccc.ufcg.edu.br>
2018-09-05 16:58:40 -0300

    mudando a estrutura

--- app.js
+++ app.js
@@ -4,12 +4,17 @@ const router = express.Router();
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
-const user = require('./routes/user');
-const index = require('./routes/index');
-const admin = require('./routes/admin');
+const user = require('./user/userRoute');
+const index = require('./index/indexRoute');
+const admin = require('./admin/adminRoute');
+const listOfBooks = require('/listOfBooks/listOfBooksRoute');
 
 app.use('/', index);
 app.use('/user', user);
 app.use('/admin', admin);
+app.use('/listOfBooks', listOfBooks);
+
+
+app.use('/imagem', express.static(__dirname+'/static'));
 
 module.exports = app;
