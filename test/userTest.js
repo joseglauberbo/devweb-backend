97da264586d4678ccc38b612b68475680b695ca6
José Glauber <jose.oliveira@ccc.ufcg.edu.br>
2018-10-09 01:01:40 -0300

    n sei se tao certos esses testes

--- test/userTest.js
+++ test/userTest.js
@@ -137,3 +137,48 @@ describe('/GET/:id user', function() {
         });
     });
 });
+
+describe('/PUT/:id user', function(){
+    it('Deve atualizar um usuário de acordo com um id', function(done){
+      var user1 = {
+        "username" : "joseglauber",
+        "email" : "hahaha@gmail.com",
+        "password": "hahahhehe"
+        }
+      user1.save(function(error, user1){
+              chai.request(server)
+              .put('/user/' + user1.id)
+              .send({
+                  username : "glauberbraz",
+                  email : "hahaha@gmail.com",
+                  password: "hahahhehe"
+                })
+              .end(function(error, res){
+                  res.should.have.status(200);
+                  res.body.should.be.a('object');
+                  res.body.should.have.property('message').eql('Usuário Atualizado com Sucesso');
+              done();
+          });
+      });
+  });
+});
+
+describe('/DELETE/:id user', function(){
+    it('Deve excluir um usuário de acordo com o id', function(done){
+        var user3 = {
+            "username" : "matheus",
+            "email" : "hehehe@gmail.com",
+            "password": "aaaaaaaaaa"
+        }
+      user3.save(function(error, user3){
+              chai.request(server)
+              .delete('/usuario/' + user3.id)
+              .end(function(error, res){
+                  res.should.have.status(200);
+                  res.body.should.be.a('object');
+                  res.body.should.have.property('message').eql('Usuário excluído com Sucesso!');
+                done();
+          });
+      });
+  });
+});
