cfd9c80b83f1b5d229362e1fdee51c55953baac5
José Glauber <jose.oliveira@ccc.ufcg.edu.br>
2018-10-09 00:23:54 -0300

    subindo mais testes para usuários

--- test/userTest.js
+++ test/userTest.js
@@ -17,6 +17,7 @@ describe('User', function(){
     });
 });
 
+//iniciando o sistema com nenhum usuário cadastrado
 describe('/GET user', function() {
     it('Deve retornar todos os meus usuários', function(done) {
         chai.request(app)
@@ -25,6 +26,113 @@ describe('/GET user', function() {
             res.should.have.status(200);
             res.body.should.be.a('array');
             res.body.length.should.be.eql(0);
+        done();
+        });
+    });
+});
+
+//adicionando um usuário no sistema com tudo desejado. o esperado é q se cadastre
+describe('/POST user', function(){
+    it("Deve retornar o POST do user criado", function(done){
+       var user1 = {
+            "username" : "joseglauber",
+            "email" : "hahaha@gmail.com",
+            "password": "hahahhehe"
+        }
+        chai.request(app)
+        .post('/user')
+        .send(user1)
+        .end(function(error, res) {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            done();
+        });
+    });
+});
+
+//verificando se existe um usuário cadastrado no sistema
+describe('/GET user', function() {
+    it('Deve retornar todos os meus usuários', function(done) {
+        chai.request(app)
+        .get('/user')
+        .end(function(error, res) {
+            res.should.have.status(200);
+            res.body.should.be.a('array');
+            res.body.length.should.be.eql(1);
+        done();
+        });
+    });
+});
+
+//adicionando usuario sem username, o esperado é que não cadastre.
+describe('/POST user', function(){
+    it("Deve retornar o POST do user criado", function(done){
+       var user1 = {
+            "email" : "hahaha@gmail.com",
+            "password": "hahahhehe"
+        }
+        chai.request(app)
+        .post('/user')
+        .send(user1)
+        .end(function(error, res) {
+            res.should.have.status(400);
+            res.body.should.be.a('object');
+            res.body.should.have.property('erros');
+            res.body.erros.should.have.property('username');
+            done();
+        });
+    });
+});
+
+//adicionando usuario repetido, o esperado é que não cadastre.
+describe('/POST user', function(){
+    it("Deve retornar o POST do user criado", function(done){
+       var user1 = {
+            "username" : "joseglauber",
+            "email" : "hahaha@gmail.com",
+            "password": "hahahhehe"
+        }
+        chai.request(app)
+        .post('/user')
+        .send(user1)
+        .end(function(error, res) {
+            res.should.have.status(400);
+            res.body.should.be.a('object');
+            done();
+        });
+    });
+});
+
+//verificando se existe um usuário cadastrado no sistema
+describe('/GET user', function() {
+    it('Deve retornar todos os meus usuários', function(done) {
+        chai.request(app)
+        .get('/user')
+        .end(function(error, res) {
+            res.should.have.status(200);
+            res.body.should.be.a('array');
+            res.body.length.should.be.eql(1);
+        done();
+        });
+    });
+});
+
+describe('/GET/:id user', function() {
+    it('Deve retornar um usuário pelo id dado', function(done) {
+        var user2 = {
+            "username" : "samara",
+            "email" : "samara@gmail.com",
+            "password": "ssss"
+        }
+        chai.request(app)
+        .get('/user/' + user2.id)
+        .send(user2)
+        .end(function(error, res) {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            res.body.should.have.property('username');
+               res.body.should.have.property('email');
+               res.body.should.have.property('password');
         done();
         });
     });
