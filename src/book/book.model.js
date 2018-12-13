55d5fed0d9f71b5cf28d63c3437d50681b39bd13
Glauber <jose.oliveira@ccc.ufcg.edu.br>
2018-12-13 15:54:06 -0300

    aaaaaaaaaa

--- src/book/book.model.js
+++ src/book/book.model.js
@@ -2,32 +2,23 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var BookSchema = new mongoose.Schema({
-	id: string,
-	etag: string,
-	volumeInfo: {
-		title: string,
-		subtitle: string,
-		authors: [
-			string
-		],
-		publisher: string,
-		publishedDate: string,
-		description: string,
-		pageCount: integer,
-		printType: string,
-		imageLinks: {
-			smallThumbnail: string,
-			thumbnail: string,
-			small: string,
-			medium: string,
-			large: string,
-			extraLarge: string
-		},
-		language: string,},
-		usuario_id: {
-			type: mongoose.SchemaTypes.ObjectId,
-			ref: 'User',
-			required: true},
+
+	nome: {
+		type: String,
+		required: true
+	},
+	descricao: {
+		type: String,
+		required: true
+	},
+	imagem: {
+		type: String,
+		required: true
+	},
+	genero: {
+		type: String,
+		required: true
+	}
 });
 
 var Book = mongoose.model('Book', UserSchema);
